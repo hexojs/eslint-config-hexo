58ce61ae63b7cc72b1ef5a4471b76e739c93ccc9
segayuu <segayuu@gmail.com>
2018-07-20 09:48:16 +0900

    Enable eslint-plugin-prettier

--- eslint.js
+++ eslint.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:prettier/recommended'],
   rules: {
     // override recomennded
     'no-console': 'off',
