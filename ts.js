7162b1ce10ff1c81ddde3d0a5cccde29ee16cfe9
Percy <kecrily@gmail.com>
2021-07-13 21:07:08 +0800

    feat: add typescript support for eslint

---
+++ ts.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    './eslint.js',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    'node/no-unsupported-features/es-syntax': ['error', { 'ignores': ['modules'] }],
+    'node/no-missing-import': ['error', { 'tryExtensions': ['.js', '.ts'] }]
+  },
+  env: {
+    mocha: true
+  }
+};
