3194fff41b3e9d252d168ef8a96302ba485120e1
yoshinorin <yoshinorin.net@outlook.com>
2024-10-31 00:56:09 +0900

    add `eslint-plugin-mocha`

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const eslint = require('./eslint.js');
 const globals = require('globals');
+const mochaPlugin = require('eslint-plugin-mocha');
 
 const testConfig = {
   languageOptions: {
@@ -12,7 +13,17 @@ const testConfig = {
   },
 };
 
+const mochaConfig = {
+  ...mochaPlugin.configs.flat.recommended,
+  rules: {
+    "mocha/no-mocha-arrows": 0,
+    "mocha/handle-done-callback": 0,
+    "mocha/max-top-level-suites": 0,
+  }
+};
+
 module.exports = [
   ...eslint,
   testConfig,
+  mochaConfig,
 ];
