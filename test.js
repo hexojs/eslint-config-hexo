69f9bdb5c3a217571e0bab08fb840789e7f4a6ea
yoshinorin <yoshinorin.net@outlook.com>
2025-01-15 08:49:44 +0900

    merge objects

--- test.js
+++ test.js
@@ -3,27 +3,21 @@ const globals = require('globals');
 const mochaPlugin = require('eslint-plugin-mocha');
 
 const testConfig = {
+  ...mochaPlugin.configs.flat.recommended,
   languageOptions: {
     globals: {
       ...globals.mocha
     },
   },
   rules: {
-    'no-unused-expressions': 'off'
-  },
-};
-
-const mochaConfig = {
-  ...mochaPlugin.configs.flat.recommended,
-  rules: {
+    'no-unused-expressions': 0,
     "mocha/no-mocha-arrows": 0,
     "mocha/handle-done-callback": 0,
     "mocha/max-top-level-suites": 0,
-  }
+  },
 };
 
 module.exports = [
   ...eslint,
-  testConfig,
-  mochaConfig,
+  testConfig
 ];
